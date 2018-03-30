2ee9cb0cb33cc6fc7bcf2586f5b13cddd2f041f3
Oskar Mendel <brokenprogrammer@gmail.com>
2018-03-30 03:46:36 +0200

    Added initial basic types. Close #8

---
+++ src/types.hpp
@@ -0,0 +1,29 @@
+#ifndef BRK_TYPES_HPP
+#define BRK_TYPES_HPP
+
+enum BasicTypes {
+    //TODO: Class, struct, enum, pointer, reference, function.. Oskar Mendel 2018-03-30
+
+    BASIC_NONE,
+    BASIC_VOID,
+
+    BASIC_INT8,
+    BASIC_UINT8,
+    BASIC_INT16,
+    BASIC_UINT16,
+    BASIC_INT32,
+    BASIC_UINT32,
+    BASIC_INT64,
+    BASIC_UINT64,
+
+    BASIC_FLOAT32,
+    BASIC_FLOAT64,
+
+    BASIC_BYTE,
+    BASIC_CHAR,
+    BASIC_STRING,
+
+    BASIC_MAX
+};
+
+#endif //BRK_TYPES_HPP
